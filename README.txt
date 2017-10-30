f0f6fbd0ee594a9de951b23e35a909e8537b409b
r.bepari <bepari.rakib@gmail.com>
2017-10-30 21:07:05 +0100

    Ajout de la fonction de calcul factorielle.

--- README.txt
+++ README.txt
@@ -1,1 +1,2 @@
 Ce projet a pour but d'écrire un script permettant de faire des calculs mathématiques.
+- Ajout de la fonction de calcul factorielle.
--- calculs.py
+++ calculs.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
+def factorielle(nbe):
+	iteration = 1
+	nbeIter   = nbe
+	while iteration != nbeIter:
+		nbe = nbe * iteration
+		iteration = iteration + 1
+	print(nbe)
+
 def main():
-    pass
+    factorielle(5)
 
 if __name__ == '__main__':
     main()
